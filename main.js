9ba03a819a290913721446d36f2eb6f542e0106f
Dan Song <blueintelli@gmail.com>
2019-11-23 20:24:21 +0900

    App 제작 - 출력정보에 대한 보안

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var url = require("url");
 var qs = require("querystring");
 var template = require("./lib/template.js");
 var path = require("path");
+var sanitizeHtml = require("sanitize-html");
 
 var app = http.createServer(function(request, response) {
   var _url = request.url;
@@ -29,15 +30,19 @@ var app = http.createServer(function(request, response) {
         var filteredId = path.parse(queryData.id).base;
         fs.readFile(`data/${filteredId}`, "utf8", function(err, description) {
           var title = queryData.id;
+          var sanitizeTitle = sanitizeHtml(title);
+          var sanitizeDescription = sanitizeHtml(description, {
+            allowedTags: ["h1"]
+          });
           var list = template.list(filelist);
           var html = template.html(
             title,
             list,
-            `<h2>${title}</h2>${description}`,
+            `<h2>${sanitizeTitle}</h2>${sanitizeDescription}`,
             `<a href="/create">create</a>
-            <a href="/update?id=${title}">update</a>
+            <a href="/update?id=${sanitizeTitle}">update</a>
             <form action="delete_process" method="post">
-              <input type="hidden" name="id" value="${title}">
+              <input type="hidden" name="id" value="${sanitizeTitle}">
               <input type="submit" value="delete">
             </form>`
           );
