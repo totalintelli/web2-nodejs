8eab96d883f507c6cb62086fb245cee1824f1765
Dan Song <blueintelli@gmail.com>
2019-11-23 20:02:17 +0900

    App 제작 - 입력정보에 대한 보안

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var fs = require("fs");
 var url = require("url");
 var qs = require("querystring");
 var template = require("./lib/template.js");
+var path = require("path");
 
 var app = http.createServer(function(request, response) {
   var _url = request.url;
@@ -25,7 +26,8 @@ var app = http.createServer(function(request, response) {
       });
     } else {
       fs.readdir("./data", function(error, filelist) {
-        fs.readFile(`data/${queryData.id}`, "utf8", function(err, description) {
+        var filteredId = path.parse(queryData.id).base;
+        fs.readFile(`data/${filteredId}`, "utf8", function(err, description) {
           var title = queryData.id;
           var list = template.list(filelist);
           var html = template.html(
@@ -83,7 +85,8 @@ var app = http.createServer(function(request, response) {
     });
   } else if (pathname === "/update") {
     fs.readdir("./data", function(error, filelist) {
-      fs.readFile(`data/${queryData.id}`, "utf8", function(err, description) {
+      var filteredId = path.parse(queryData.id).base;
+      fs.readFile(`data/${filteredId}`, "utf8", function(err, description) {
         var title = queryData.id;
         var list = template.list(filelist);
         var html = template.html(
@@ -132,7 +135,8 @@ var app = http.createServer(function(request, response) {
     request.on("end", function() {
       var post = qs.parse(body);
       var id = post.id;
-      fs.unlink(`data/${id}`, function(error) {
+      var filteredId = path.parse(id).base;
+      fs.unlink(`data/${filteredId}`, function(error) {
         response.writeHead(302, { Location: `/` });
         response.end();
       });
